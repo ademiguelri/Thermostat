be1a08d40b6c72e633b32cf26514c0260d62df3b
ademiguel <ademiguel@vicomtech.org>
2022-02-03 08:49:44 +0100

    Transitions created

--- python/stateMachine.py
+++ python/stateMachine.py
@@ -3,24 +3,33 @@ from pickle import TRUE
 from transitions import Machine
 import random
 
-class machine:
+class transitions:
 
     STATESLIST = ['start', 'warming', 'cooling', 'off']
-    LOOP = True
-    list1 = [0, 1, 2]
 
     def __init__(self):
-        self.machine = Machine(model = self, states=machine.STATESLIST, initial  = 'start')
+        self.machine = Machine(model = self, states=transitions.STATESLIST, initial  = 'start')
         self.temp = 16
+        self.LOOP = True
 
-        self.machine.add_transition(trigger='init', source='start', dest='warming', after='tempChange(temp,1)')
-        self.machine.add_transition(trigger='tenp_max', source='warming', dest='cooling', after='tempChange(temp,0)')
-        self.machine.add_transition(trigger='tenp_min', source='cooling', dest='warming', after='tempChange(temp , 1)')
+        self.machine.add_transition(trigger='init', source='start', dest='warming', after='tempChange')
+        self.machine.add_transition(trigger='temp_max', source='warming', dest='cooling', after='tempChange')
+        self.machine.add_transition(trigger='temp_min', source='cooling', dest='warming', after='tempChange')
         self.machine.add_transition(trigger='power_off', source='*', dest='OFF', after='stopMachine')
 
-    def tempChange(self, operation):
-        if operation == 1:
-            self.temp += random.choice(list1)
+    def tempChange(self):
+        if self.machine.state == 'start':
+            self.machine.init()
+        elif self.machine.state == 'warming':
+            if self.temp < 21:
+                self.temp += random()
+            else:
+                self.machine.temp_max()
+        elif self.machine.state == 'cooling':
+            if self.temp > 21:
+                self.temp -= random()
+            else:
+                self.machine.temp_min()
 
     def stopMachine(self):
-        LOOP = False
+        self.LOOP = False
