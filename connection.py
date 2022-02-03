f0d472592beb71e9c353505827fbe8de6e4439cb
ademiguel <ademiguel@vicomtech.org>
2022-02-03 14:21:00 +0100

    Connection created with docker, problems to solve inserting values

--- connection.py
+++ connection.py
@@ -1,35 +1,53 @@
 from time import sleep
+import datetime
+from warnings import catch_warnings;
 import psycopg2
 import python.config as config
 import python.stateMachine as stateMachine
 
 CONNECTION = "postgres://"+config.username+":"+config.password+"@"+config.host+":"+config.port+"/"+config.dbName
-query_create_table = "CREATE TABLE auto (dateTime TIMESTAMP, info INT);"
+query_create_table = "CREATE TABLE therm (datetime TIMESTAMP, temp FLOAT);"
+query_create_hypertable = "SELECT create_hypertable('therm', 'datetime');"
+drop_table = "DROP TABLE therm;"
 
 def main():
     with psycopg2.connect(CONNECTION) as conn:
         cursor = conn.cursor()
 
     cursor = conn.cursor()
-    cursor.execute(query_create_table)
-    conn.commit()
-    cursor.close()
+    try:
+        cursor.execute(query_create_table)
+        conn.commit()
+        cursor.execute(query_create_hypertable)
+        conn.commit()
+    except:
+        cursor.execute(drop_table)
+    finally:
+        conn.commit()
 
 
-    #initialize state machine
-    thermostat = stateMachine.thermostat()
-    print("---Thermostat created---")
-    print("Startig state: "+ thermostat.machine.get_state(thermostat.state).name)
-    while thermostat.LOOP:
-        print("---Temp Change---")
-        thermostat.tempChange()
-        sleep(1)
-        if thermostat.state == 'start':
-            thermostat.initialize()
-        elif thermostat.temp > 23:
-            thermostat.temp_max()
-        elif thermostat.temp < 19:
-            thermostat.temp_min()
+        #initialize state machine
+        thermostat = stateMachine.thermostat()
+        print("---Thermostat created---")
+        print("Startig state: "+ thermostat.machine.get_state(thermostat.state).name)
+        while thermostat.LOOP:
+            print("---Temp Change---")
+            thermostat.tempChange()
+         
+            if thermostat.state == 'start':
+                thermostat.initialize()
+            elif thermostat.temp > 23:
+                thermostat.temp_max()
+            elif thermostat.temp < 19:
+                thermostat.temp_min()
+
+            ct = datetime.datetime.now()
+            print(ct)
+            cursor.execute("INSERT INTO therm (datetime, temp) VALUES ("+str(ct)+","+str(thermostat.temp)+")")
+            conn.commit()
+            sleep(1)
+
+        cursor.close()
 
 if __name__ == "__main__":
-    main()
+   main()
