61f7f419ac6723841b8195249b3e02c7a8c005ce
ademiguel <ademiguel@vicomtech.org>
2022-02-03 13:27:16 +0100

    Create data from python to postgres

--- connection.py
+++ connection.py
@@ -3,19 +3,19 @@ import psycopg2
 import python.config as config
 import python.stateMachine as stateMachine
 
-#CONNECTION = "postgres://"+config.username+":"+config.password+"@"+config.host+":"+config.port+"/"+config.dbName
-#query_create_table = "CREATE TABLE auto (dateTime TIMESTAMP, info INT);"
+CONNECTION = "postgres://"+config.username+":"+config.password+"@"+config.host+":"+config.port+"/"+config.dbName
+query_create_table = "CREATE TABLE auto (dateTime TIMESTAMP, info INT);"
 
 def main():
-#    with psycopg2.connect(CONNECTION) as conn:
-#        cursor = conn.cursor()
+    with psycopg2.connect(CONNECTION) as conn:
+        cursor = conn.cursor()
 
-#    cursor = conn.cursor()
-#    cursor.execute(query_create_table)
-#    conn.commit()
-#    cursor.close()
+    cursor = conn.cursor()
+    cursor.execute(query_create_table)
+    conn.commit()
+    cursor.close()
 
-    
+
     #initialize state machine
     thermostat = stateMachine.thermostat()
     print("---Thermostat created---")
